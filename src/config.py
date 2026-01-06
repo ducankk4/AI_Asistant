17a2437441f262275a04cb80ea2ba3cc5e64be1a
{ADZ} <{nguyenducanhthachda@gmail.com}>
2026-01-06 17:49:13 +0700

    update chunksize, chunkoverlap

--- src/config.py
+++ src/config.py
@@ -15,20 +15,21 @@ RESPONSE_MODEL = "meta-llama/llama-4-maverick-17b-128e-instruct"
 REWRITE_MODEL = "meta-llama/llama-4-maverick-17b-128e-instruct"
 ANALYSIS_MODEL = "meta-llama/llama-4-maverick-17b-128e-instruct"
 
-FINAL_LAPTOP_DATA = r"crawl_data\final_data\laptop.txt"
-FINAL_CSBH_DATA = r"crawl_data\final_data\csbh.txt"
-FINAL_CSDT_DATA = r"crawl_data\final_data\csdt.txt"
-FINAL_CSVC_DATA = r"crawl_data\final_data\csvc.txt"
+class DataPaths:
+    FINAL_LAPTOP_DATA = r"src\crawl_data\final_data\laptop.txt"
+    FINAL_CSBH_DATA = r"src\crawl_data\final_data\csbh.txt"
+    FINAL_CSDT_DATA = r"src\crawl_data\final_data\csdt.txt"
+    FINAL_CSVC_DATA = r"src\crawl_data\final_data\csvc.txt"
 
 class RAG_config:
     top_k_result: int = 3
-    chunk_size: int = 1000
-    chunk_overlap: int = 150
+    chunk_size: int = 512
+    chunk_overlap: int = 100
     persist_directory: str = r"src\vector_store\chroma_collections"
-
-LAPTOP_COLLECTION_NAME: str = "laptop_collection"
-CSBH_COLLECTION_NAME: str = "csbh_collection"
-CSDT_COLLECTION_NAME: str = "csdt_collection"
-CSVC_COLLECTION_NAME: str = "csvc_collection"
+class CollectionNames:
+    LAPTOP_COLLECTION_NAME: str = "laptop_collection"
+    CSBH_COLLECTION_NAME: str = "csbh_collection"
+    CSDT_COLLECTION_NAME: str = "csdt_collection"
+    CSVC_COLLECTION_NAME: str = "csvc_collection"
 
 rag_config = RAG_config()
